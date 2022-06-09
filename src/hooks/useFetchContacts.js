1d9851b000a3873036ad6737c527249b2ea2decb
Tiffany <yang.ding@hotmail.com>
2022-06-09 15:24:22 -0700

    updated useFetchContacts hook to add localStorage contacts

--- src/hooks/useFetchContacts.js
+++ src/hooks/useFetchContacts.js
@@ -1,17 +1,25 @@
 import { useEffect, useState } from 'react';
-import { mockContacts } from '../utils/mockContacts'
+import { mockContacts } from '../utils/mockContacts';
+import { getContacts } from '../utils/contacts';
 
 export const useFetchContacts = () => {
-    // initialize contacts state with empty []
-    const [contacts, setContacts] = useState([])
+  // initialize contacts state with empty []
+  const [contacts, setContacts] = useState([]);
 
-    // on component mount / page render, 
-    // setContacts with mockContacts derived from utils
-    useEffect(() => {
-        setContacts(mockContacts)
-    }, [])
+  // on component mount / page render,
+  // setContacts with mockContacts derived from utils
+  useEffect(() => {
+    // get contacts from localStorage
+    const storedContacts = getContacts();
 
-    // return contacts from this custom hook 
-    // holds whatever information mockContacts holds
-    return [contacts];
-}
+    // merge mockContacts and storedContacts
+    // into one state
+    setContacts([...mockContacts, ...storedContacts]);
+
+  }, []);
+
+  // return contacts from this custom hook
+  // holds information from
+  // mockContacts and storedContacts combined
+  return [contacts];
+};
