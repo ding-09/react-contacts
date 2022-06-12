2492112af5a50f4fad14f282e7a52c82211d1678
Tiffany <yang.ding@hotmail.com>
2022-06-12 14:13:16 -0700

    added conditional rendering of Contact components based on auth context

--- src/components/App.js
+++ src/components/App.js
@@ -5,23 +5,27 @@ import ContactsPage from './contacts';
 import ContactDetailsPage from './contact-details';
 import ContactCreatePage from './contact-create';
 import { ContactsProvider } from '../providers/ContactsProvider';
-import { AuthProvider, useAuth } from '../providers/AuthProvider';
+import { useAuth } from '../providers/AuthProvider';
 
 const App = () => {
   // const [contacts, setContacts] = useState([]);
 
   // const [contacts, setContacts] = useFetchContacts();
-  
+
+  const authContext = useAuth();
+
   return (
-    <AuthProvider>
+    <>
       <LoginPage />
       <RegisterPage />
-      <ContactsProvider>
-        <ContactsPage />
-        <ContactDetailsPage />
-        <ContactCreatePage />
-      </ContactsProvider>
-    </AuthProvider>
+      {authContext.loggedIn && (
+        <ContactsProvider>
+          <ContactsPage />
+          <ContactDetailsPage />
+          <ContactCreatePage />
+        </ContactsProvider>
+      )}
+    </>
   );
 };
 
